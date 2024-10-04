e395a490364dc79e2bcd2de0a6302c38bec840f9
melodyy <andrewmscott824@gmail.com>
2024-10-04 16:53:59 -0400

    Door work!!!
    Guys I did it

--- godotProject/Door/door.gd
+++ godotProject/Door/door.gd
@@ -2,7 +2,3 @@ extends Area2D
 
 func _ready():
 	add_to_group("door")
-
-func _on_body_entered(body: Node2D) -> void:
-	if Input.is_action_pressed("jump"):
-		print("Happy Birthday!!")
--- godotProject/Player/objects/player.gd
+++ godotProject/Player/objects/player.gd
@@ -8,6 +8,15 @@ extends CharacterBody2D
 
 @export var inventory: Array[String]
 
+var levelno = 1
+
+func _ready():
+	print("ready")
+	await get_tree().create_timer(.1).timeout
+	var doors = get_tree().get_nodes_in_group("door")
+	for i in len(doors):
+		doors[i].body_entered.connect(touch_door)
+
 func _physics_process(delta: float) -> void:
 	var input_axis: float = Input.get_axis(&"move_left", &"move_right")
 	velocity.x = move_toward(velocity.x, move_speed * input_axis, acceleration * delta)
@@ -30,3 +39,24 @@ func add_item(item: String) -> void:
 
 func clear_inventory() -> void:
 	inventory.clear()
+
+func touch_door(_body):
+	# Waits a tiny bit to maybe fade in and out or whatever we want to do.
+	await get_tree().create_timer(.1).timeout
+	load_n_unload()
+	await get_tree().create_timer(.1).timeout
+
+func load_n_unload():
+	# Remove the current level
+	var level = get_node("/root/Root/Level1")
+	get_node("/root/Root").remove_child(level)
+	level.call_deferred("free")
+	print("part 1 done")
+	
+	levelno += 1
+
+# Add the next level
+	var next_level_resource = load("res://level" + str(levelno) + ".tscn")
+	var next_level = next_level_resource.instantiate()
+	get_node("/root/Root").add_child(next_level)
+	print("part 2 done")
