1b6bfb833a1eefe9f0ae397203e558e31c8d1f82
Tenzenmaster <160365410+Tenzenmaster@users.noreply.github.com>
2024-11-24 00:31:31 -0500

    merged tenzen_test_level into root

--- godotProject/Player/objects/new_player.gd
+++ godotProject/Player/objects/new_player.gd
@@ -5,6 +5,9 @@ extends CharacterBody2D
 enum State { IDLE, RUN, AIR, ATTACK }
 
 
+signal player_death
+
+
 @export var move_speed: float
 @export var jump_velocity: float
 @export var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
