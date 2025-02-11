1751a59e93a05f2ff5c75db07e0030607a497429
Tenzenmaster <160365410+Tenzenmaster@users.noreply.github.com>
2025-02-11 11:13:27 -0500

    door fix

--- godotProject/Door/NewDoor/new_door.gd
+++ godotProject/Door/NewDoor/new_door.gd
@@ -3,10 +3,17 @@ class_name NewDoor
 extends Node2D
 
 
+## PackedScene of the room this door leads to
+@export var target_room: PackedScene
+## The ID of this door.
+## Another door with a target_id that matches this door's id will lead to this door.
 @export var id := -1
+## The ID of the door that this door will lead to.
 @export var target_id := -1
 
 
+## Connect body_entered signals here.
+## Will emit Global.door_entered if body is a NewPlayer.
 func _on_area_2d_body_entered(body: PhysicsBody2D) -> void:
 	if body is NewPlayer:
 		Global.door_entered.emit(self)
