44b6e434d8fd54633c15a5add817ea49b16890a2
Tenzenmaster <160365410+Tenzenmaster@users.noreply.github.com>
2025-02-18 16:18:55 -0500

    global inventory

--- godotProject/World/pickup/pickup.gd
+++ godotProject/World/pickup/pickup.gd
@@ -1,17 +1,15 @@
 class_name Pickup
 extends Area2D
 
-@export var item_name = ""
 
-func _ready():
-	# Method to get player might change later tbh -Tenzen
-	var player = get_tree().get_nodes_in_group("player")[0]
-	# If player already has pickup's name in its inventory, delete pickup -Tenzen
-	if player.has_item(item_name):
+@export var item_name := ""
+
+
+func _ready() -> void:
+	body_entered.connect(_on_body_entered)
+
+
+func _on_body_entered(body: PhysicsBody2D) -> void:
+	if body is NewPlayer:
+		Global.add_to_inventory(item_name)
 		queue_free()
-		
-func get_picked_up():
-	queue_free()
-
-func _on_area_entered(area) -> void:
-	pass # probably going to delete
--- godotProject/global.gd
+++ godotProject/global.gd
@@ -6,3 +6,16 @@ signal door_entered(door: NewDoor)
 signal enemy_death(enemy: Enemy)
 signal player_health_updated(health: int)
 signal player_death
+signal item_added(item_name: String)
+
+
+var inventory: Array[String] = []
+
+
+func add_to_inventory(item_name: String) -> void:
+	if item_name in inventory:
+		push_warning("%s already in inventory" % item_name)
+		return
+	
+	inventory.push_back(item_name)
+	item_added.emit(item_name)
