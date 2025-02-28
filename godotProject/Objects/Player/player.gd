376b7b285f33258484464f646bb87e374a6eef87
Tenzenmaster <160365410+Tenzenmaster@users.noreply.github.com>
2025-02-28 13:12:24 -0500

    more player stuff

--- godotProject/Objects/Player/player.gd
+++ godotProject/Objects/Player/player.gd
@@ -6,6 +6,9 @@ var ground_state := GroundPlayerState.new(self)
 var air_state := AirPlayerState.new(self)
 var state: PlayerState = ground_state
 
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+@onready var animation_tree: AnimationTree = $AnimationTree
+
 
 func _ready() -> void:
 	transition(ground_state)
